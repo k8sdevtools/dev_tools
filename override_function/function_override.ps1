1c30d62aea61f59d3efd7341160c763d83b54530
k8sdevtools <the.dev.snippets@gmail.com>
2025-02-12 13:54:11 -0600

    simplify function

--- override_function/function_override.ps1
+++ override_function/function_override.ps1
@@ -2,25 +2,12 @@ param (
     [bool]$Override = $false
 )
 
-function script {
-    param (
-        [bool]$Override
-    )
+$validationFile = "validation.text"
 
-    $validationFile = "validation.text"
-
-    if (Test-Path $validationFile -and -not $Override) {
-        Write-Output "Validation file exists. Exiting script."
-        exit
-    }
-
-    Write-Output "Running script..."
-
-    Out-File -FilePath $validationFile -Encoding utf8 -InputObject "script ran"
-
-    Write-Host "Validation file created."
+# Check if the validation file exists and apply the override logic
+if (Test-Path $validationFile -and -not $Override) {
+    Write-Output "Validation file exists. Exiting script."
+    exit
 }
-
-
 # Call the function and pass the Override parameter
 powershell.exe -ExecutionPolicy Bypass -File myscript.ps1 -Override $true
