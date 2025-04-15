59d4ea61d422e723c5b201586ade7519cc8bd676
k8sdevtools <the.dev.snippets@gmail.com>
2025-04-15 11:50:30 -0500

    add cron job

--- ca_bundle/patch-ca-bundle.sh
+++ ca_bundle/patch-ca-bundle.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-export WEBHOOK_NAME=gitlab-workspaces-kubernetes-webhook
-export WEBHOOK_NAMESPACE=default
-
 CA_BUNDLE=$(base64 -w0 < certs/ca.crt)
 
-patch_webhook_yaml() {
-  local file="$1"
-  echo "Patching $file with CA bundle"
+for file in dev/manifests/cluster-config/*config.yaml; do
+  echo "Patching $file"
   yq e ".webhooks[].clientConfig.caBundle = \"$CA_BUNDLE\"" -i "$file"
-}
-
-patch_webhook_yaml dev/manifests/cluster-config/mutating.config.yaml
-patch_webhook_yaml dev/manifests/cluster-config/validating.config.yaml
+done
